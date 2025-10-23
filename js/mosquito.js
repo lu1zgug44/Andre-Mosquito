4424c7c0f3e81a26618807e24e9a21c13b4c68fa
LUIZ GUSTAVO CARVALHO DA SILVA COSTA <0000111775618xsp@aluno.educacao.sp.gov.br>
2025-10-23 14:55:58 -0300

    parte 1 feita

---
+++ js/mosquito.js
@@ -0,0 +1,16 @@
+// criação de variáveis
+
+let largura = 0
+let altura = 0
+
+function ajustarTela(){
+    largura = window.innerWidth
+    altura = window.innerHeight
+    console.log(largura, altura)
+}
+
+ajustarTela()
+
+function desenharMosquito(){
+    
+}
